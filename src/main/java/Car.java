f60fb55d7c7c42de345bf7b6406ca53fba459c52
정태혁 <jtaeh57@gmail.com>
2020-06-08 16:36:07 +0900

    refactor: Car
    - remove magic number

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,15 +1,17 @@
 import java.util.Random;
 
 public class Car {
+    private static final int DEFAULT_GO_PERCENT = 40;
+
     private final int canMoveRate;
     private final Random random;
 
-    public Car(){
-        this(40);
+    public Car() {
+        this(DEFAULT_GO_PERCENT);
     }
 
-    Car(int canMoveRate) {
-        this.canMoveRate = canMoveRate;
+    Car(int canGoPercent) {
+        this.canMoveRate = canGoPercent;
         this.random = new Random();
     }
 
